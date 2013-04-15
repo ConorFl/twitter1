4929b40df097984ca3c110e8a957b139f5a35a55
Apprentice <apprentice@devbootcamp.com>
2013-04-15 15:58:06 -0700

    twitter 2

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,12 +1,30 @@
 get '/' do
+  erb :index
 end
 
 get '/:username' do
   @user = TwitterUser.find_or_create_by_name(params[:username])
-  if @user.tweets.empty?
-    @user.fetch_tweets!
-  elsif @user.tweets_stale?
-    @user.fetch_tweets!
+  if @user.tweets.empty? || @user.tweets_stale?
+    erb :render
+  else
+    @tweets = @user.tweets.limit(10)
+    erb :index
+  end 
+end
+
+post '/:username' do
+  @user = TwitterUser.find_by_name(params[:username])
+  @user.fetch_tweets!
+  @tweets = @user.tweets.limit(10)
+  erb :display_partial, layout: false
+end
+
+get '/refresh/:username' do
+  @user = TwitterUser.find_or_create_by_name(params[:username])
+  if @user.tweets.empty? || @user.tweets_stale?
+    erb :render_partial, layout: false
+  else
+    @tweets = @user.tweets.limit(10)
+    erb :display_partial, layout: false
   end
-  erb :index
 end
--- app/models/twitter_user.rb
+++ app/models/twitter_user.rb
@@ -5,13 +5,11 @@ class TwitterUser < ActiveRecord::Base
     most_recent_tweet = self.tweets.first.tweet_time
     oldest_tweet = self.tweets.last.tweet_time
     avg_time = (most_recent_tweet - oldest_tweet) / self.tweets.length.to_f
-
-
     self.tweets.last.updated_at < (Time.now - avg_time)
   end
 
   def fetch_tweets!
-    @tweets = Twitter.user_timeline(self.name, :count => 10)
+    @tweets = Twitter.user_timeline(self.name, :count => 100)
     self.tweets.destroy_all
     @tweets.each do |tweet|
       self.tweets << Tweet.create(text: tweet.text, tweet_time: tweet.created_at)
