a01bc40f03301be2c08a21014c4af53d55a4f6d2
Apprentice <apprentice@devbootcamp.com>
2013-04-15 17:30:00 -0700

    make it look nice

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-
+  // var container = $('.container');
+  // if (container.val().length == 0) {
+  //   container.hide();
+  // }
+  // $(document).on('click', 'input[name="show_tweets"]', function() {
+  //   container.show();
+  // })
 });
